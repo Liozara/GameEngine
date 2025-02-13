e16485c4f968532e291a029500474fecdbc3bcfe
Fenrir68 <loukaboutet@gmail.com>
2025-02-13 20:36:42 +0100

    [MOD]
    create window in main

--- Engine/EngineProject/main.cpp
+++ Engine/EngineProject/main.cpp
@@ -1,8 +1,33 @@
 #include <iostream>
+#include "glad/glad.h"
+#include "GLFW/glfw3.h"
 
 //TODO Faire un truc bien inchallah
 
 int main() {
-	std::cout << "hello wsh" << std::endl;
-	std::cin.get();
+	glfwInit();
+
+	//def la version de opengl utilisé
+	glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+	glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
+	//selectionne selement les modules les plus modernes (GLFW_OPENGL_COMPAT_PROFILE pour inclure les obsolètes)
+	glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+
+	GLFWwindow* window = glfwCreateWindow(500, 500, "GameEngine", NULL, NULL);
+	if (window == NULL)
+	{
+		std::cout << "Failed to create window" << std::endl;
+		glfwTerminate();
+		return -1;
+	}
+	glfwMakeContextCurrent(window);
+
+	while (!glfwWindowShouldClose(window))
+	{
+		//prendre en compte les actions effectué sur la fenetre
+		glfwPollEvents();
+	}
+
+	glfwTerminate();
+	return 0;
 }
